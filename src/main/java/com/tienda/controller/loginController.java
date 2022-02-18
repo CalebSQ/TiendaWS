a934b44f5b9e6dfc71e97077669b8d524226941c
Caleb Solis <caleb.solis.q@gmail.com>
2022-02-18 10:29:28 -0600

    Configuracion de que Index sea clase principal

--- src/main/java/com/tienda/controller/loginController.java
+++ src/main/java/com/tienda/controller/loginController.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *
  * @author caleb
  */
+@Controller
 public class loginController {
     
+    @GetMapping("/")
+    public String index(){
+        return "login";
+    }
 }
--- src/main/java/com/tienda/controller/personasController.java
+++ src/main/java/com/tienda/controller/personasController.java
@@ -1,18 +1,10 @@
 package com.tienda.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
 /**
  *
  * @author caleb
  */
-@Controller
 public class personasController {
-    
-    @GetMapping("/")
-    public String index(){
-        return "login";
-    }
+
     
 }
