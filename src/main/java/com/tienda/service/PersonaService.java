be5d2fb89546f49e4b98eda876d5f619188ff937
Caleb Solis <caleb.solis.q@gmail.com>
2022-02-22 19:22:41 -0600

    Modificacion de Personas Controller (Clase 5)

--- src/main/java/com/tienda/service/PersonaService.java
+++ src/main/java/com/tienda/service/PersonaService.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.tienda.service;
+package com.tienda.Service;
 
-/**
- *
- * @author caleb
- */
-public class PersonaService {
+import com.tienda.entity.Persona;
+import com.tienda.repository.PersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonaService implements iPersonaService{
+
+    //Inyeccion de dependencias
+    @Autowired
+    private PersonaRepository personaRepository;
     
+    @Override
+    public List<Persona> getAllPerson() {
+        return (List<Persona>)personaRepository.findAll();
+    }
+
+    @Override
+    public void savePerson(Persona persona) {
+        personaRepository.save(persona);
+    }
+
+    @Override
+    public Persona getPersonById(long id) {
+        return personaRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(long id) {
+        personaRepository.deleteById(id);
+    }
 }
