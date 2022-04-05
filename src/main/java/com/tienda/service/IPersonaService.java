b3407ff0d5df60b71bfaf07b954a265831696988
Caleb Solis <caleb.solis.q@gmail.com>
2022-04-04 20:53:37 -0600

    Semana 11

--- src/main/java/com/tienda/service/IPersonaService.java
+++ src/main/java/com/tienda/service/IPersonaService.java
@@ -4,10 +4,10 @@ import com.tienda.entity.Persona;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface IPersonaService {
     public List<Persona> getAllPerson();
     public void savePerson(Persona persona);
     public Persona getPersonById(long id);
     public void delete(long id);
+    public Persona findByNombre (String nombre);
 }
